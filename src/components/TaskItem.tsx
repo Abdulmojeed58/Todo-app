e95a52e75f9989ba8803ea66ce82d1b0980761dc
Abdulmojeed <mojeedayoola58@gmail.com>
2023-09-11 00:51:56 +0100

    fix: text style

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -29,10 +29,10 @@ const formatedDate = `${date.month}/${date.day}/${date.year}`
   return (
     <Card className="bg-[#F9FAFB] border-b border-b-skin-border-gray-200 hover:bg-skin-gray-bg">
       <div className="flex items-center justify-between cursor-pointer" onClick={onClick}>
-        <div  className="flex items-center gap-3">
+        <div  className="flex items-center md:gap-3">
           <input type="checkbox" className="checkbox" {...inputProps} />
           <div className="text-skin-text-primary">
-            <h3 className={`font-medium text-[14px] ${completed ? 'line-through text-skin-text-done' : ''}`}>{title}</h3>
+            <h3 style={{whiteSpace: 'normal'}} className={`font-medium text-[14px] ${completed ? 'line-through text-skin-text-done' : ''}`}>{title}</h3>
             <p className={`font-normal text-[14px] ${completed ? 'line-through text-skin-text-done' : ''}`}>{startTime + ' - ' + endTime}</p>
           </div>
         </div>
